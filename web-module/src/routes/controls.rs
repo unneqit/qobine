2735ccb927567d21e0b9f73aeac12dcc7a5a5089
Sofus Addington <sofus@addington.dk>
2026-06-14 14:06:43 +0200

    fix a htmx in web

--- web-module/src/routes/controls.rs
+++ web-module/src/routes/controls.rs
@@ -5,9 +5,15 @@ use axum::{Router, extract::State, response::IntoResponse, routing::get};
 use crate::AppState;
 
 pub fn routes() -> Router<std::sync::Arc<crate::AppState>> {
-    Router::new().route("/controls", get(controls))
+    Router::new()
+        .route("/controls", get(controls))
+        .route("/controls/content", get(controls_partial))
 }
 
 async fn controls(State(state): State<Arc<AppState>>) -> impl IntoResponse {
     state.render("controls.html", &())
+}
+
+async fn controls_partial(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    state.render("controls-content.html", &())
 }
